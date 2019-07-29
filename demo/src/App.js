15613d0c6c4a72b4800177187a9ae5d14ae6d8ff
Neha Gupta <nehagupta0595@gmail.com>
2019-07-29 13:38:41 +0530

    Show Login button during initial rendering and when login fail

--- demo/src/App.js
+++ demo/src/App.js
@@ -8,30 +8,39 @@ class App extends React.Component {
     super(props);
     this.state = {
       message: 'Click here to login with Google',
+      showGoogleLoginButton: true,
     };
   }
 
   responseGoogleSuccess = response => {
-    this.setState({ message: 'Congratulations Login Successful' });
+    this.setState({
+      message: 'Congratulations Login Successful',
+      showGoogleLoginButton: false,
+    });
   };
 
   responseGoogleFail = response => {
-    this.setState({ message: 'Sorry! Login UnSuccessful! Try Again..' });
+    this.setState({
+      message: 'Sorry! Login UnSuccessful! Try Again..',
+      showGoogleLoginButton: true,
+    });
   };
 
   render() {
-    const { message } = this.state;
+    const { message, showGoogleLoginButton } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{message}</p>
-          <GoogleLogin
-            clientId="693482368528-668ai0igsqmseiucrvumr7igg36altce.apps.googleusercontent.com"
-            buttonText="Continue Login with Google"
-            onSuccess={this.responseGoogleSuccess}
-            onFailure={this.responseGoogleFail}
-          />
+          {showGoogleLoginButton && (
+            <GoogleLogin
+              clientId="693482368528-668ai0igsqmseiucrvumr7igg36altce.apps.googleusercontent.com"
+              buttonText="Continue Login with Google"
+              onSuccess={this.responseGoogleSuccess}
+              onFailure={this.responseGoogleFail}
+            />
+          )}
         </header>
       </div>
     );
