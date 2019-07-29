cbf9c05f14481ec7561156f00cf6692ae55289a4
Neha Gupta <nehagupta0595@gmail.com>
2019-07-29 13:33:18 +0530

    Update Readme.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Assignment
-Login with Google
+Login with Google - It Container Continue With Google button to login with google.If Login Succedded it prints Login Successful message otherwise it shows error and show Continue with gooogle button again.
+
+The Code of Google Login Exists inside  demo -> src -> App.js
+
+The style file is App.css.And the assets used in it are in asset folder.
--- demo/src/App.js
+++ demo/src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/googleLogo.png';
 import './App.css';
+import { GoogleLogin } from 'react-google-login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Click here to login with Google',
+    };
+  }
+
+  responseGoogleSuccess = response => {
+    this.setState({ message: 'Congratulations Login Successful' });
+  };
+
+  responseGoogleFail = response => {
+    this.setState({ message: 'Sorry! Login UnSuccessful! Try Again..' });
+  };
+
+  render() {
+    const { message } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{message}</p>
+          <GoogleLogin
+            clientId="693482368528-668ai0igsqmseiucrvumr7igg36altce.apps.googleusercontent.com"
+            buttonText="Continue Login with Google"
+            onSuccess={this.responseGoogleSuccess}
+            onFailure={this.responseGoogleFail}
+          />
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
